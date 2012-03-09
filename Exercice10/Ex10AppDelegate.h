28d9e401a73fbb60f9affe074725f96bd475f433
NLelouche <nahuel.lelouche@threepillarglobal.com>
2012-03-09 10:12:57 -0300

    Initial Commit

---
+++ Exercice10/Ex10AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Ex10AppDelegate.h
+//  Exercice10
+//
+//  Created by TPG on 3/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Ex10AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
